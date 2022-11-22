40efcc948e3996be702c43c64637a1590c13acf0
koyaaoyama <aoyama.ko0920@gmail.com>
2022-11-23 00:31:45 +0900

    add component LinkListItem

---
+++ components/main/LinkListItem.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from 'react'
+
+type Props = {
+  href: string
+	children?: ReactNode
+}
+
+/**
+ * Main page link list item
+ *
+ * @param {Props} { href, children }
+ * @return {*} JSX.Element
+ */
+const LinkListItem = ({ href, children }: Props) => {
+	return (
+		<li className='text-3xl text-textMainDark cursor-pointer hover:text-orange-600'>
+			<a href={href} target='_blank' rel='noreferrer'>
+				{children}
+			</a>
+		</li>
+	)
+}
+
+export default LinkListItem
