3e6f230a34edfeca9905c7b008c8b19b6f2a78f2
koyaaoyama <aoyama.ko0920@gmail.com>
2022-11-23 23:51:03 +0900

    edit tailwind.config.js. add media query xs. add bgColorLight.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
   theme: {
     // media query
     screens: {
+      xs: '400px',
       sm: '480px',
       md: '768px',
       lg: '976px',
@@ -15,6 +16,8 @@ module.exports = {
     extend: {
       colors: {
         textMainDark: '#333333',
+        bgColorLight: '#f4f5f7',
+        // bgColorLight: '#f5f5f5',
       }
     },
   plugins: [],
