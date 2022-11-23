f4abbcf6bb6678f4a22309adcc69f7358392a54e
koyaaoyama <aoyama.ko0920@gmail.com>
2022-11-23 23:55:30 +0900

    edit app.tsx. apply getLayout pattern

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
+import React, { ReactElement, ReactNode } from 'react'
+import { NextPage } from 'next'
+import { AppProps } from 'next/app'
 import '../styles/globals.css'
-import type { AppProps, } from 'next/app'
 
-export default function App({ Component, pageProps, }: AppProps) {
-  return <Component {...pageProps} />
+// https://nextjs.org/docs/basic-features/layouts
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function App({ Component, pageProps, }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page)
+
+  return getLayout(<Component {...pageProps} />)
 }
