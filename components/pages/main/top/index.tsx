89a1321bfdfda1bc8d6889275ddabc89a053480e
koyaaoyama <aoyama.ko0920@gmail.com>
2022-11-24 23:03:44 +0900

    extract main page top section into components/pages/main/top/index.tsx

--- components/pages/main/top/index.tsx
+++ components/pages/main/top/index.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 // icons
 import { BsGithub, BsLinkedin, BsFileEarmarkPdfFill } from 'react-icons/bs'
 import { HiOutlineMail } from 'react-icons/hi'
-import { GiHamburgerMenu } from 'react-icons/gi'
-import { GrClose } from 'react-icons/gr'
 
 // constants
 import { gitHubUrl } from '../../../../constants'
@@ -13,24 +12,16 @@ import { gitHubUrl } from '../../../../constants'
 // helpers
 import { isInBrowser } from '../../../../helpers/environment'
 
-// custom hooks
-import { useIsWindowSizeWider } from '../../../../hooks/useIsWindowSizeWider'
-import MobileMenu, { useMobileMenu } from '../../../common/mobileMenu'
-
 // components
-import LinkListItem from './LinkListItem'
+import LinkListItem from '../../../../components/pages/main/top/LinkListItem'
+import MainLayout from '../../../../components/layouts/MainLayout'
 
 /**
  * main page top section
  *
- * TODO: extract layout
- *
  * @return {*} JSX.Element
  */
 const Top = () => {
-  const { isMobileMenuOpen, setIsMobileMenuOpen, toggleMobileMenu } = useMobileMenu()
-
-  const { isWindowSizeWider } = useIsWindowSizeWider(768)
 
   /**
    * email icon onClick
@@ -53,87 +44,31 @@ const Top = () => {
       return false
   }
 
-  /**
-   * manage mobile hamburger menu open/close
-   *
-   */
-  const handleMobileMenuButtonOnClick = () => {
-    toggleMobileMenu()
-  }
-
-  useEffect(() => {
-    // close mobile menu if screen is expanded while mobile menu is open
-    if (isWindowSizeWider) setIsMobileMenuOpen(false)
-  }, [isWindowSizeWider])
-
   return (
-    <div>
-      <header
-        className={`
-          flex items-center justify-between bg-bgColorLight w-full h-[64px] border-b border-slate-200 space-x-11 px-8
-          fixed top-0 left-0 right-0
-          md:justify-start
-          z-[1]
-          `
-        }
-      >
-        {/* TODO: logo */}
-        <div>KOYA</div>
-
-        <div className='md:flex md:justify-between md:flex-1'>
-          {/* header nav */}
-          <nav className='hidden md:flex space-x-11 text-orange-600'>
-            <div className='flex items-center justify-center font-bold hover:text-orange-400 cursor-pointer'>
-              Skills & Experiences
-            </div>
-            <div className='flex items-center justify-center font-bold hover:text-orange-400 cursor-pointer'>
-              My Work
-            </div>
-            <div className='flex items-center justify-center font-bold hover:text-orange-400 cursor-pointer'>
-              About
-            </div>
-          </nav>
-
-          {/* resume download button */}
-          <button className={`
-              hidden
-              md:flex items-center justify-center w-40 h-8 border-2 border-orange-600 rounded-[50px]
-              text-orange-600 font-bold
-              hover:bg-orange-600 hover:text-white duration-500 hover:shadow-black hover:shadow-md
-              active:shadow-inner active:shadow-black`
-            }
-          >
-            <p>Resume</p>
-          </button>
-
-          {/* mobile menu open/close button */}
-          <button onClick={handleMobileMenuButtonOnClick} className='md:hidden'>
-            {isMobileMenuOpen ? <GrClose /> : <GiHamburgerMenu />}
-          </button>
-          {/* mobile menu contents */}
-          {/* TODO: mobile menu anchor link */}
-          <MobileMenu isOpen={isMobileMenuOpen} setMobileMenuState={setIsMobileMenuOpen} />
-        </div>
-      </header>
-
+    <>
       {/* hero container */}
-      <div className='flex items-center justify-center h-[calc(100vh_-_64px)] mt-[64px] px-5 lg:p-24'>
+      {/* lg:p-24 */}
+      <div className='flex items-center justify-center h-[calc(100vh_-_64px)] mt-[64px] p-5 lg:p-12'>
+        {/* h-[calc(100% - 96px)] */}
         <div className={`
-            flex flex-col justify-center items-center gap-10
-            h-[calc(100% - 96px)] w-full
+            flex flex-col justify-center items-center gap-6
+            h-full w-full
             px-10
             py-10
             rounded-[20px]
             bg-slate-200
             shadow-xl
             xs:py-28
+            sm:gap-6
+            lg:items-start
             lg:flex-row
             lg:py-40
             `
           }
         >
+          {/* profile photo */}
           <section>
-            <div className='w-[150px] h-[150px] lg:w-[200px] lg:h-[200px] relative'>
+            <div className='w-[150px] h-[150px] md:w-[250px] md:h-[250px] relative'>
               <Image
                 src='/images/profile/me_monotone.png'
                 layout='fill'
@@ -147,14 +82,21 @@ const Top = () => {
           <section>
             <div className='mb-3.5'>
               {/* text-[52px] */}
-              <p className='text-[45px] font-extrabold text-orange-600 md:text-[50px] lg:text-[75px]'>Koya Aoyama</p>
-              <p className='text-[45px] font-extrabold text-textMainDark md:text-[50px]'>
+              <p
+                className={
+                  `text-[45px] font-extrabold text-orange-600
+                  sm:text-[55px] md:text-[75px] lg:text-[80px] xl:text-9xl xl:mb-3.5`
+                }
+              >
+                Koya Aoyama
+              </p>
+              <p className='text-[35px] font-extrabold text-textMainDark md:text-[50px] xl:text-[70px]'>
                 Frontend <br className='hidden'/>developer
               </p>
             </div>
             {/* <p>Sometimes backend developer.</p> */}
             {/* links */}
-            <ul className='flex items-center gap-6'>
+            <ul className='flex items-center gap-6 pb-1 mb-10'>
               <LinkListItem href={gitHubUrl}>
                 <BsGithub />
               </LinkListItem>
@@ -177,14 +119,58 @@ const Top = () => {
                 </a>
               </li>
             </ul>
+            <div>
+              <p className='text-sm text-textMainDark md:text-xl'>
+                I am a Frontend developer who loves creating applications.
+              </p>
+              <p className='text-sm text-textMainDark md:text-xl'>
+                Please contact me if you are interested!
+              </p>
+            </div>
+
+            {/* use <img> tag intentionally instead of <Image /> component to avoid a strange error */}
+            {/* the error is like: 400 bad request. "url" parameter is valid but image type is not allowed. */}
+            {/* <ul className='flex'>
+              <li>
+                <img src="https://skillicons.dev/icons?i=react" alt='React icon' />
+              </li>
+              <li>
+                <img src="https://skillicons.dev/icons?i=typescript" alt='TypeScript icon' />
+              </li>
+              <li>
+                <img src="https://skillicons.dev/icons?i=nextjs" alt='Next.js icon' />
+              </li>
+              <li>
+                <img src="https://skillicons.dev/icons?i=redux" alt='Redux icon' />
+              </li>
+              <li>
+                <img src="https://skillicons.dev/icons?i=jest" alt='Jest icon' />
+              </li>
+            </ul> */}
+
+            {/* &theme=light to make icons bright */}
+            {/* <ul className='flex'>
+              <li>
+                <img src="https://skillicons.dev/icons?i=php" alt='PHP icon' />
+              </li>
+              <li>
+                <img src="https://skillicons.dev/icons?i=laravel" alt='Laravel icon' />
+              </li>
+              <li>
+                <img src="https://skillicons.dev/icons?i=mysql" alt='MySQL icon' />
+              </li>
+              <li>
+                <img src="https://skillicons.dev/icons?i=linux" alt='Linux icon' />
+              </li>
+              <li>
+                <img src="https://skillicons.dev/icons?i=aws" alt='AWS icon' />
+              </li>
+            </ul> */}
+
           </section>
         </div>
       </div>
-
-      <div>
-        TODO: here are some contents
-      </div>
-    </div>
+    </>
   )
 }
 
