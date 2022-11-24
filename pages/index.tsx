a06c8a015497b5c9497f0567eac971f70a4da13c
koyaaoyama <aoyama.ko0920@gmail.com>
2022-11-24 22:55:01 +0900

    fix main page layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,10 @@ import styles from '../styles/Home.module.css'
 
 // icons
 import { BsGithub, BsLinkedin, BsFileEarmarkPdfFill } from 'react-icons/bs'
-import { MdEmail } from 'react-icons/md'
 import { HiOutlineMail } from 'react-icons/hi'
-import { GiHamburgerMenu } from 'react-icons/gi'
-import { GrClose } from 'react-icons/gr'
+import { SiTypescript, SiRedux } from 'react-icons/si'
+import { FaReact } from 'react-icons/fa'
+import { RiReactjsLine } from 'react-icons/ri'
 
 // constants
 import { gitHubUrl } from '../constants'
@@ -68,18 +68,21 @@ const Home = () => {
 
   return (
     <>
-
       {/* hero container */}
-      <div className='flex items-center justify-center h-[calc(100vh_-_64px)] mt-[64px] px-5 lg:p-24'>
+      {/* lg:p-24 */}
+      <div className='flex items-center justify-center h-[calc(100vh_-_64px)] mt-[64px] p-5 lg:p-12'>
+        {/* h-[calc(100% - 96px)] */}
         <div className={`
-            flex flex-col justify-center items-center gap-10
-            h-[calc(100% - 96px)] w-full
+            flex flex-col justify-center items-center gap-6
+            h-full w-full
             px-10
             py-10
             rounded-[20px]
             bg-slate-200
             shadow-xl
             xs:py-28
+            sm:gap-6
+            lg:items-start
             lg:flex-row
             lg:py-40
             `
@@ -87,7 +90,7 @@ const Home = () => {
         >
           {/* profile photo */}
           <section>
-            <div className='w-[150px] h-[150px] lg:w-[200px] lg:h-[200px] relative'>
+            <div className='w-[150px] h-[150px] md:w-[250px] md:h-[250px] relative'>
               <Image
                 src='/images/profile/me_monotone.png'
                 layout='fill'
@@ -101,14 +104,21 @@ const Home = () => {
           <section>
             <div className='mb-3.5'>
               {/* text-[52px] */}
-              <p className='text-[45px] font-extrabold text-orange-600 md:text-[50px] lg:text-[75px]'>Koya Aoyama</p>
-              <p className='text-[45px] font-extrabold text-textMainDark md:text-[50px]'>
+              <p
+                className={
+                  `text-[45px] font-extrabold text-orange-600
+                  sm:text-[55px] md:text-[75px] lg:text-[80px] xl:text-9xl xl:mb-3.5`
+                }
+              >
+                Koya Aoyama
+              </p>
+              <p className='text-[35px] font-extrabold text-textMainDark md:text-[50px] xl:text-[70px]'>
                 Frontend <br className='hidden'/>developer
               </p>
             </div>
             {/* <p>Sometimes backend developer.</p> */}
             {/* links */}
-            <ul className='flex items-center gap-6'>
+            <ul className='flex items-center gap-6 pb-1 mb-10'>
               <LinkListItem href={gitHubUrl}>
                 <BsGithub />
               </LinkListItem>
@@ -131,6 +141,54 @@ const Home = () => {
                 </a>
               </li>
             </ul>
+            <div>
+              <p className='text-sm text-textMainDark md:text-xl'>
+                I am a Frontend developer who loves creating applications.
+              </p>
+              <p className='text-sm text-textMainDark md:text-xl'>
+                Please contact me if you are interested!
+              </p>
+            </div>
+
+            {/* use <img> tag intentionally instead of <Image /> component to avoid a strange error */}
+            {/* the error is like: 400 bad request. "url" parameter is valid but image type is not allowed. */}
+            {/* <ul className='flex'>
+              <li>
+                <img src="https://skillicons.dev/icons?i=react" alt='React icon' />
+              </li>
+              <li>
+                <img src="https://skillicons.dev/icons?i=typescript" alt='TypeScript icon' />
+              </li>
+              <li>
+                <img src="https://skillicons.dev/icons?i=nextjs" alt='Next.js icon' />
+              </li>
+              <li>
+                <img src="https://skillicons.dev/icons?i=redux" alt='Redux icon' />
+              </li>
+              <li>
+                <img src="https://skillicons.dev/icons?i=jest" alt='Jest icon' />
+              </li>
+            </ul> */}
+
+            {/* &theme=light to make icons bright */}
+            {/* <ul className='flex'>
+              <li>
+                <img src="https://skillicons.dev/icons?i=php" alt='PHP icon' />
+              </li>
+              <li>
+                <img src="https://skillicons.dev/icons?i=laravel" alt='Laravel icon' />
+              </li>
+              <li>
+                <img src="https://skillicons.dev/icons?i=mysql" alt='MySQL icon' />
+              </li>
+              <li>
+                <img src="https://skillicons.dev/icons?i=linux" alt='Linux icon' />
+              </li>
+              <li>
+                <img src="https://skillicons.dev/icons?i=aws" alt='AWS icon' />
+              </li>
+            </ul> */}
+
           </section>
         </div>
       </div>
