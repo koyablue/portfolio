a33eca108b5e829083eef9f7a9ef875aa9da3c0a
koyaaoyama <aoyama.ko0920@gmail.com>
2022-11-27 19:55:32 +0900

    common card component

---
+++ components/common/Card.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from 'react'
+
+// hooks
+import { useStyleJoin } from '../../hooks/useStyleJoin'
+
+type Props = {
+	className?: string | string[]
+	children?: ReactNode
+}
+
+/**
+ * common card component
+ *
+ * @param {Props} { className, children }
+ * @return {*} JSX.Element
+ */
+const Card = ({ className = '', children }: Props) => {
+	const { styleJoin } = useStyleJoin()
+
+	const styleToApply = Array.isArray(className) ? styleJoin(className) : className
+
+	return (
+		<div className={`bg-slate-200 rounded-[20px] shadow-xl ${styleToApply}`}>
+			{children}
+		</div>
+	)
+}
+
+export default Card
