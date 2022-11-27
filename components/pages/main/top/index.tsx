ef13b874cc0e4eff8f7485771f29400e503aca03
koyaaoyama <aoyama.ko0920@gmail.com>
2022-11-27 23:49:33 +0900

    fix top section style. mobile view, landscape view. still unstable though...

--- components/pages/main/top/index.tsx
+++ components/pages/main/top/index.tsx
@@ -46,36 +46,35 @@ const Top = () => {
   return (
     <>
       {/* hero container */}
-      {/* lg:p-24 */}
-      {/* h-[calc(100vh_-_64px)] */}
       <div className={`
           flex items-center justify-center
-          h-[calc(100%_-_64px)] w-full
+          h-[calc(100%_-_64px)]
+          w-full
+          overflow-auto
           mt-[64px] p-5
-          xs:h-[calc(100vh_-_64px)]
-          lg:p-12`
+          md:h-[calc(100vh_-_64px)]
+          md:p-12`
         }
       >
-        {/* h-[calc(100% - 96px)] */}
+        {/* card */}
+        {/* lg:py-40 */}
+        {/* lg:items-start */}
+        {/* lg:flex-row */}
+        {/* h-full */}
         <div className={`
             flex flex-col justify-center items-center gap-6
-            h-full w-full
+            w-full
             px-10
             py-10
             rounded-[20px]
             bg-slate-200
             shadow-xl
-            xs:py-28
-            sm:gap-6
-            lg:items-start
-            lg:flex-row
-            lg:py-40
             `
           }
         >
           {/* profile photo */}
-          <section>
-            <div className='w-[120px] h-[120px] md:w-[250px] md:h-[250px] relative'>
+          <section className=''>
+            <div className='w-[120px] h-[120px] md:w-[200px] md:h-[200px] relative'>
               <Image
                 src='/images/profile/me_monotone.png'
                 layout='fill'
@@ -86,18 +85,13 @@ const Top = () => {
           </section>
 
           {/* intro and links */}
-          <section>
+          <section className='flex flex-col items-center'>
             <div className='mb-3.5'>
               {/* text-[52px] */}
-              <p
-                className={
-                  `text-[45px] font-extrabold text-orange-600
-                  sm:text-[55px] md:text-[75px] lg:text-[80px] xl:text-9xl xl:mb-3.5`
-                }
-              >
+              <p className='text-[35px] text-center font-extrabold text-orange-600 md:text-[75px]'>
                 Koya Aoyama
               </p>
-              <p className='text-[35px] font-extrabold text-textMainDark md:text-[50px] xl:text-[70px]'>
+              <p className='text-[25px] text-center font-extrabold text-textMainDark md:text-[50px]'>
                 Frontend <br className='hidden'/>developer
               </p>
             </div>
@@ -127,10 +121,10 @@ const Top = () => {
               </li>
             </ul>
             <div>
-              <p className='text-xs text-textMainDark xs:text-sm md:text-xl'>
+              <p className='text-sm text-textMainDark md:text-xl'>
                 I am a Frontend developer who loves creating applications.
               </p>
-              <p className='text-xs text-textMainDark xs:text-sm md:text-xl'>
+              <p className='text-sm text-textMainDark md:text-xl'>
                 Please contact me if you are interested!
               </p>
             </div>
