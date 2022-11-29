d1886b9c0bb273c096f6c879e1522b84ad786788
koyaaoyama <aoyama.ko0920@gmail.com>
2022-11-29 22:33:27 +0900

    components/pages/main/common/MainPageSectionTitle.tsx

---
+++ components/pages/main/common/MainPageSectionTitle.tsx
@@ -0,0 +1,20 @@
+type Props = {
+	as?: React.ElementType
+	title: string
+}
+
+/**
+ *
+ *
+ * @param {Props} { as: Element = 'h2', title }
+ * @return {*} JSX.Element
+ */
+const MainPageSectionTitle = ({ as: Element = 'h2', title }: Props) => {
+	return (
+		<Element className='text-textMainDark text-xl font-bold text-center mb-8 md:text-4xl'>
+			{title}
+		</Element>
+	)
+}
+
+export default MainPageSectionTitle
