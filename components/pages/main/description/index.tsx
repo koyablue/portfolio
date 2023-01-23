88d589fbbf08efdd38b7aabf313d051f933c7b7c
koyaaoyama <aoyama.ko0920@gmail.com>
2023-01-23 15:01:05 +0900

    description

--- components/pages/main/description/index.tsx
+++ components/pages/main/description/index.tsx
@@ -2,13 +2,13 @@ const Description = () => {
   return (
     <div className='flex justify-center p-12 md:p-16 '>
       <div className='flex flex-col  text-appDarkNavy text-sm md:text-xl'>
-        <p className='leading-5 md:leading-10'>
+        <p className='leading-loose'>
           I am a Web developer with 3 and a half years experience who loves creating applications.
         </p>
-        <p className='leading-5 md:leading-10'>
+        <p className='leading-loose'>
           Skilled in TypeScript, React, Next.js, HTML, CSS, PHP, Laravel and MySQL.
         </p>
-        <p className='leading-5 md:leading-10'>
+        <p className='leading-loose'>
           Please contact me if you are interested!
         </p>
       </div>
