7483a5f69173fdfa281f015809902b323845d836
koyaaoyama <aoyama.ko0920@gmail.com>
2023-01-30 19:15:16 +0900

    implement cookie util functions

---
+++ constants/cookieName.ts
@@ -0,0 +1,1 @@
+export const colorThemeCookieName = 'colorTheme' as const
---
+++ services/cookie.ts
@@ -0,0 +1,48 @@
+import Cookies, { CookieAttributes } from 'js-cookie'
+
+// constants
+import { colorThemeConfig } from '../constants/colorTheme'
+import { colorThemeCookieName } from '../constants/cookieName'
+
+// types
+import { ColorTheme, isColorTheme } from '../types/theme'
+
+/**
+ * Set cookie
+ *
+ * @param {string} name
+ * @param {string} value
+ * @param {CookieAttributes} [options]
+ */
+const setCookie = (name: string, value: string, options?: CookieAttributes) => {
+  Cookies.set(name, value, options)
+}
+
+/**
+ * Get cookie by given cookie name
+ *
+ * @param {string} name
+ * @return {*}  {string}
+ */
+const getCookie = (name: string): string => Cookies.get(name) || ''
+
+/**
+ * Set cookie for color theme
+ *
+ * @param {ColorTheme} value
+ */
+export const setColorThemeCookie = (value: ColorTheme) => {
+  setCookie(colorThemeCookieName, value)
+}
+
+/**
+ * Get color theme config from cookie
+ *
+ * @return {*}  {ColorTheme}
+ */
+export const getColorThemeCookie = (): ColorTheme => {
+  const cookieVal = getCookie(colorThemeCookieName)
+  if (!cookieVal || !isColorTheme(cookieVal)) return colorThemeConfig.light
+
+  return cookieVal
+}
