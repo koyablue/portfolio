eb78ec9c31bbc5cfb280433cd0a455bfd67a3e96
koyaaoyama <aoyama.ko0920@gmail.com>
2023-02-01 17:45:57 +0900

    add LayoutWrapper and SubLayout

---
+++ components/layouts/LayoutWrapper.tsx
@@ -0,0 +1,34 @@
+import { ReactNode } from 'react'
+
+// redux
+import { useAppSelector } from '../../stores/hooks'
+import { selectIsThemeSet } from '../../stores/slices/colorThemeSlice'
+
+// components
+import ColorThemeProvider from './includes/ColorThemeProvider'
+
+type Props = {
+  children: ReactNode
+}
+
+/**
+ *
+ *
+ * @return {*} JSX.Element
+ */
+const LayoutWrapper = ({ children }: Props) => {
+  const isThemeSet = useAppSelector(selectIsThemeSet)
+
+  return (
+    <div>
+      <ColorThemeProvider />
+      {isThemeSet &&
+        <>
+          {children}
+        </>
+      }
+    </div>
+  )
+}
+
+export default LayoutWrapper
---
+++ components/layouts/SubLayout.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from 'react'
+
+// components
+import Header from './includes/Header'
+import LayoutWrapper from './LayoutWrapper'
+
+type Props = {
+  children?: ReactNode
+}
+
+/**
+ * Layout for the pages other than the top page.
+ *
+ * @param {Props} { children }
+ * @return {*} JSX.Element
+ */
+const SubLayout = ({ children }: Props) => {
+  return (
+    <LayoutWrapper>
+      <Header />
+      {children}
+    </LayoutWrapper>
+  )
+}
+
+export default SubLayout
