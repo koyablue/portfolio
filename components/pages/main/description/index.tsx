dc7e8146afa9a90cef9af5f7679501278bca1539
koyaaoyama <aoyama.ko0920@gmail.com>
2023-07-10 14:20:08 -0700

    Change light theme color of description section

--- components/pages/main/description/index.tsx
+++ components/pages/main/description/index.tsx
@@ -7,22 +7,27 @@ import GitHubCalendar from 'react-github-calendar';
  */
 const Description = () => {
   return (
-    <div className='flex justify-center box-border py-12  bg-indigo-900 md:py-16 dark:bg-clrDarkNavy'>
+    <div className='
+      flex justify-center
+      box-border py-12
+      md:px-28
+      md:bg-slate-200 md:py-16 dark:bg-clrDarkNavy'
+    >
       <div className='flex flex-col flex-1 items-center
       text-clrWhite
       tracking-wide
        text-sm
-       bg-clrDarkNavy
+       bg-slate-800
        bg-clip-content
+       md:rounded
        md:text-lg
-       md:px-28
        dark:bg-slate-800
        dark:text-clrWhiteOpa'
       >
         <div className='p-8 leading-loose'>
           <div className='mb-6'>
             <p className='text-5xl dark:text-slate-100'>Hi 👋</p>
-            <p className='text-4xl dark:text-slate-100'>I&apos;m <span className='dark:text-clrOrange'>Koya Aoyama</span> 🧑🏻‍💻</p>
+            <p className='text-4xl dark:text-slate-100'>I&apos;m <span className='text-clrOrange'>Koya Aoyama</span> 🧑🏻‍💻</p>
           </div>
 
           <p>
