3e195f105acf3ff9f184e3d885cfdbc920ade3e8
koyaaoyama <aoyama.ko0920@gmail.com>
2024-02-11 01:09:54 -0800

    Fix: LinkButton dark mode style

--- components/common/projects/projectCard/buttons/LinkButton.tsx
+++ components/common/projects/projectCard/buttons/LinkButton.tsx
@@ -25,7 +25,7 @@ const LinkButton = ({ label = '', href = '', children }: LinkButtonProps) => {
         active:shadow-none
         ${href && 'dark:text-slate-200 dark:border-slate-200'}
         ${!href && 'bg-gray-200 text-gray-500 cursor-not-allowed shadow-none active:opacity-100 dark:bg-gray-300 dark:text-gray-400 dark:border-gray-300'}
-        ${href && 'hover:bg-indigo-400/10 hover:text-indigo-500 dark:hover:shadow-[3.5px_3.5px_0_0_rgb(226,232,240)] dark:active:opacity-60 dark:active:shadow-none'}`
+        ${href && 'hover:bg-indigo-400/10 hover:text-indigo-500 dark:hover:bg-sky-400/10 dark:hover:text-sky-400'}`
       }
       disabled={Boolean(href)}
     >
